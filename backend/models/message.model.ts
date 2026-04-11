ded76e54ff7e43384d5e8aef511be12ca4765fb2
sin-colors <shingo6651inagaki2302@i3.gmobb.jp>
2026-04-12 01:15:42 +0900

    Messageモデルをグループチャット用に修正するためreceiverフィールドを削除

--- backend/models/message.model.ts
+++ backend/models/message.model.ts
@@ -7,11 +7,6 @@ const messageSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
-    receiverId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
     message: {
       type: String,
       required: true,
