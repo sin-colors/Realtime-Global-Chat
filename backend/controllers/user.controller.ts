9c724ac6d5fd3e4253022193c9f4aa0916eed060
sin-colors <shingo6651inagaki2302@i3.gmobb.jp>
2026-04-18 15:56:23 +0900

    Get Users For Sidebarエンドポイントの作成

---
+++ backend/controllers/user.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import User from "../models/user.model";
+
+export async function getUsersForSidebar(req: Request, res: Response) {
+  try {
+    if (!req.user)
+      return res.status(401).json({ error: "ログインしてください" });
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+    res.status(200).json(filteredUsers);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log(
+        "getUsersForSidebar controllerでエラーが発生しました",
+        err.message,
+      );
+    } else {
+      console.log("getUsersForSidebar controllerでエラーが発生しました", err);
+    }
+    res
+      .status(500)
+      .json({ error: "getUsersForSidebar関数を実行中にエラーが発生しました" });
+  }
+}
--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes";
-import connectToMongoDB from "./db/connectToMongoDB";
 import messageRoutes from "./routes/message.routes";
+import userRoutes from "./routes/user.routes";
+import connectToMongoDB from "./db/connectToMongoDB";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -11,12 +12,13 @@ const PORT = process.env.PORT || 5000;
 
 // リクエストのbodyからJSONを取り出すために必要なミドルウェア
 app.use(express.json());
-//
+// リクエストボディからクッキーを取り出すのに必要なミドルウェア
 app.use(cookieParser());
 
-// ミドルウェアの追加
+// ミドルウェアの追加（ルート関係）
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req: Request, res: Response) => {
 //   res.send("Hello world!");
