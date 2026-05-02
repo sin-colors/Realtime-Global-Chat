b410f8f985ccc13070caa3a83521af96c46914cc
sin-colors <shingo6651inagaki2302@i3.gmobb.jp>
2026-05-02 22:53:42 +0900

    useLoginフックをuseMutationを使ったコードにリファクタリング

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -1,11 +1,11 @@
 import type { LoginType } from "@/lib/schema/authSchema";
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 function useLogin() {
   const queryClient = useQueryClient();
-  async function login(values: LoginType) {
-    const loginPromise = (async () => {
+  const mutation = useMutation({
+    mutationFn: async (values: LoginType) => {
       const response = await fetch("/api/auth/login", {
         method: "POST",
         headers: {
@@ -17,9 +17,15 @@ function useLogin() {
         const errorData = await response.json();
         throw new Error(errorData.error);
       }
-      const loginData = await response.json();
-      queryClient.setQueryData(["authUser"], loginData);
-    })();
+      return response.json();
+    },
+    onSuccess: (data) => {
+      queryClient.setQueryData(["authUser"], data);
+    },
+  });
+
+  async function login(values: LoginType) {
+    const loginPromise = mutation.mutateAsync(values);
     toast.promise(loginPromise, {
       loading: "ログインしています。。。",
       success: "ログインしました",
@@ -27,6 +33,41 @@ function useLogin() {
     });
     await loginPromise;
   }
-  return { login };
+  return { login, isLoading: mutation.isPending };
 }
 export default useLogin;
+
+//-----------React QueryのuseMutationを使ってリファクタリングする前のコード------------------
+
+// import type { LoginType } from "@/lib/schema/authSchema";
+// import { useQueryClient } from "@tanstack/react-query";
+// import { toast } from "sonner";
+
+// function useLogin() {
+//   const queryClient = useQueryClient();
+//   async function login(values: LoginType) {
+//     const loginPromise = (async () => {
+//       const response = await fetch("/api/auth/login", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(values),
+//       });
+//       if (!response.ok) {
+//         const errorData = await response.json();
+//         throw new Error(errorData.error);
+//       }
+//       const loginData = await response.json();
+//       queryClient.setQueryData(["authUser"], loginData);
+//     })();
+//     toast.promise(loginPromise, {
+//       loading: "ログインしています。。。",
+//       success: "ログインしました",
+//       error: (err) => err.message || "予期せぬエラーが発生しました",
+//     });
+//     await loginPromise;
+//   }
+//   return { login };
+// }
+// export default useLogin;
