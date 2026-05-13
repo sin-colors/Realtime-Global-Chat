fef87ad476f452eaea91dc3dbcc6f906bfc5b45e
sin-colors <shingo6651inagaki2302@i3.gmobb.jp>
2026-05-14 03:19:12 +0900

    generateTokenの保存漏れ。sameSiteをnoneに指定

--- backend/utils/generateToken.ts
+++ backend/utils/generateToken.ts
@@ -9,7 +9,7 @@ function generateTokenAndSetCookie(userId: Types.ObjectId, res: Response) {
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "none",
     secure: process.env.NODE_ENV !== "development",
   });
 }
