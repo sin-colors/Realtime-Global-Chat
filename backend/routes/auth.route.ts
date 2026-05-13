d8b872b2f02b6ea53e462b7021fc2c03f1cd81f0
sin-colors <shingo6651inagaki2302@i3.gmobb.jp>
2026-05-13 22:03:38 +0900

    tsconfig.jsonの修正

--- backend/routes/auth.route.ts
+++ backend/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controllers";
+import { login, logout, signup } from "../controllers/auth.controller";
 
 const router = express.Router();
 
