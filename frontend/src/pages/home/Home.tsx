1cd5cea544a24156b150ed5b16f5bad450b3f896
sin-colors <shingo6651inagaki2302@i3.gmobb.jp>
2026-05-15 01:48:37 +0900

    モバイルヘッダーを上部に固定

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -4,15 +4,17 @@ import MessageContainer from "@/components/messages/MessageContainer";
 
 function Home() {
   return (
-    <div className="flex h-screen w-full max-w-4xl overflow-hidden bg-clip-padding backdrop-filter sm:h-112.5 sm:rounded-lg sm:bg-gray-400/0 sm:backdrop-blur-lg md:h-137.5">
+    <div className="flex h-dvh w-full max-w-4xl overflow-hidden bg-clip-padding backdrop-filter sm:h-112.5 sm:rounded-lg sm:bg-gray-400/0 sm:backdrop-blur-lg md:h-137.5">
       <div className="hidden sm:block sm:min-w-48">
         <Sidebar showMessageMenu={true} />
       </div>
-      <div className="flex w-full flex-col md:min-w-112.5">
-        <div className="block sm:hidden">
+      <div className="flex h-full w-full flex-1 flex-col md:min-w-112.5">
+        <div className="sticky top-0 z-10 block sm:hidden">
           <MobileHeader showMessageMenu={true} />
         </div>
-        <MessageContainer />
+        <div className="flex-1 overflow-y-auto">
+          <MessageContainer />
+        </div>
       </div>
     </div>
   );
